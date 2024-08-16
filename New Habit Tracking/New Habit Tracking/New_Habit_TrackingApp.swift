1df2bc222743e0b89d107e30976b1a7ec2d26c82
Bennett Nguyen <bennett.nguyen@gmail.com>
2024-08-16 09:37:56 -0700

    base of app

---
+++ New Habit Tracking/New Habit Tracking/New_Habit_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Habit_TrackingApp.swift
+//  New Habit Tracking
+//
+//  Created by Bennett Nguyen on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct New_Habit_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
